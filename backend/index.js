feed722d1c91364f600b3ea412ce8ad6c8c7c9df
Aftab Khan <kaft9865@gmail.com>
2026-02-22 13:20:14 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  // origin: "https://netflix-clone-mern-stack-ck54.vercel.app", // Strictly no trailing slash
-   origin: "*",
+  origin: (origin, callback) => {
+    // Agar request bina origin ke hai (jaise Postman) ya origin match karta hai
+    const allowedOrigins = [
+     "https://netflix-clone-mern-stack-ck54.vercel.app",
+      "http://localhost:5173"
+    ];
+    
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
+  allowedHeaders: ["Content-Type", "Authorization"]
 };
+
+// app.use(cors(corsOptions));
 app.use(cors(corsOptions));
 
 // routing
