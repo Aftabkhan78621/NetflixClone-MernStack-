ca31cdd9770c509f029b864707af8f13a1b38332
Aftab Khan <kaft9865@gmail.com>
2026-02-22 15:56:09 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -18,19 +18,19 @@ const corsOptions = {
   origin: (origin, callback) => {
     // Agar request bina origin ke hai (jaise Postman) ya origin match karta hai
     const allowedOrigins = [
-     "https://netflix-clone-mern-stack-ck54.vercel.app",
-      "http://localhost:5173"
+      "https://netflix-clone-mern-stack-ednq.vercel.app",
+      "http://localhost:5173",
     ];
-    
+
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 // app.use(cors(corsOptions));
